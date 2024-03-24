91292b134c0811ff99fd22428d8a8502b232b33c
VivithRK <vivithkanchi25@gmail.com>
2024-03-25 00:01:55 +0530

    modelling completed

--- models/cart/cart.js
+++ models/cart/cart.js
@@ -1,27 +1,37 @@
 const mongoose = require("mongoose");
 
-
-const cartSchema = new mongoose.Schema({
-    user_id: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-        required: true,
+const cartSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
     },
-    product_id: {
-        type: String,
-        required: true,
-    },
+    products: [
+      {
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Product",
+          required: true,
+        },
+        quantity: {
+          type: Number,
+          required: true,
+          default: 1,
+        },
+      },
+    ],
     total_amount: {
-        type: String,
-        required: true,
-    }
-}
-    ,
-    {
-        timestamps: true
-    }
-
+      type: Number,
+      required: true,
+      default: 0,
+    },
+  },
+  {
+    timestamps: true,
+  }
 );
 
 const Cart = mongoose.model("Cart", cartSchema);
+
 module.exports = Cart;
--- models/users/users.js
+++
@@ -1,2 +0,0 @@
-// * add the model here
-// *the model name should be User
