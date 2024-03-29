f3d6346974b965d59c46a86a917a6142686bdfcb
VivithRK <vivithkanchi25@gmail.com>
2024-03-29 18:30:08 +0530

    removed userid

--- server.js
+++ server.js
@@ -5,15 +5,19 @@ const session = require("express-session");
 const MongoStore = require("connect-mongo");
 require("dotenv").config();
 require("./config/dbconnect");
+const userRoutes = require("./routes/userRoute");
+const bodyParser = require("body-parser");
 
 // !file import
 
 // * routes
 
 const app = express();
+app.use(express.json());
+
+app.use("/users", userRoutes);
 
 // *middlewares
-app.use(express.json());
 
 // !server listen
 const port = process.env.port || 9000;
